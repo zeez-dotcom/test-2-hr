9ce981800feb1feb70699022ecbd1335af16e509
zeez-dotcom <80801321+zeez-dotcom@users.noreply.github.com>
2025-10-05 15:45:34 +0300

    test: guard vitest text encoder polyfill

--- HRPayMaster/vitest.setup.ts
+++ HRPayMaster/vitest.setup.ts
@@ -1,4 +1,30 @@
-import { TextDecoder, TextEncoder } from 'util';
+import { TextDecoder as NodeTextDecoder, TextEncoder as NodeTextEncoder } from 'util';
 
-(globalThis as unknown as { TextEncoder: typeof TextEncoder }).TextEncoder = TextEncoder;
-(globalThis as unknown as { TextDecoder: typeof TextDecoder }).TextDecoder = TextDecoder;
+const globalScope = globalThis as typeof globalThis & {
+  TextEncoder?: typeof NodeTextEncoder;
+  TextDecoder?: typeof NodeTextDecoder;
+};
+
+const isEncoderMissing = typeof globalScope.TextEncoder === 'undefined';
+const isEncoderInvalid = (() => {
+  if (isEncoderMissing) {
+    return false;
+  }
+
+  try {
+    return !(new globalScope.TextEncoder().encode('') instanceof Uint8Array);
+  } catch {
+    return true;
+  }
+})();
+
+if (isEncoderMissing || isEncoderInvalid) {
+  const nodeEncoder = NodeTextEncoder;
+  if (new nodeEncoder().encode('') instanceof Uint8Array) {
+    globalScope.TextEncoder = nodeEncoder;
+  }
+}
+
+if (typeof globalScope.TextDecoder === 'undefined') {
+  globalScope.TextDecoder = NodeTextDecoder;
+}
